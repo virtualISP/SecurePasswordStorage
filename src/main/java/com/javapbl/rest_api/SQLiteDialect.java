e4cce069108f1d767cd4d4c9295b819070f8b6f3
an0nym0u5 <an0nym0u5@localhost.localdomain>
2024-06-28 08:52:14 +0530

    Initial commit

--- src/main/java/com/javapbl/rest_api/SQLiteDialect.java
+++ src/main/java/com/javapbl/rest_api/SQLiteDialect.java
@@ -4,6 +4,7 @@ import org.hibernate.dialect.Dialect;
 
 public class SQLiteDialect extends Dialect {
 
+    @SuppressWarnings("deprecation")
     public SQLiteDialect() {
         super();
     }
--- src/main/java/com/javapbl/rest_api/controller/DatabaseAPIService.java
+++ src/main/java/com/javapbl/rest_api/controller/DatabaseAPIService.java
@@ -1,14 +1,19 @@
 package com.javapbl.rest_api.controller;
-import com.javapbl.rest_api.model.ApiResponse;
-import com.javapbl.rest_api.service.UserManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javapbl.rest_api.model.ApiResponse;
+import com.javapbl.rest_api.service.UserManager;
 
 @RestController
 @RequestMapping("/user")
-@CrossOrigin(origins = "http://localhost:63342")
+//@CrossOrigin(origins = "http://localhost:63342")
+//@CrossOrigin(origins = "*")
 public class DatabaseAPIService {
 
     @Autowired
